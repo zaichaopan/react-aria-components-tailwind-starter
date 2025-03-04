7fcef39cb2314d17fa53d1a7ad09024adaa656e5
zaichaopan <zaichaopan@gmail.com>
2025-03-03 20:48:23 -0800

    Fix skeletion

--- stories/Skeleton.stories.tsx
+++ stories/Skeleton.stories.tsx
@@ -11,12 +11,12 @@ export default meta;
 
 export const BasicExample = () => {
   return (
-    <div className="flex flex-col space-y-8">
+    <div className="flex flex-col gap-y-8">
       {Array.from(Array(4)).map((_, index) => {
         return (
           <div key={index} className="flex items-center space-x-2">
             <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
+            <div className="flex flex-col gap-y-2">
               <Skeleton className="h-2 w-[100px]" />
               <Skeleton className="h-2 w-[250px]" />
               <Skeleton className="h-2 w-[220px]" />
